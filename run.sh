b6b012aff5a3110bd4d939a31a937b30e85c86be
JaeikBae <jaeik@node1.cluster.local>
2023-12-13 20:50:55 +0900

    FIX: 네트워크 설정에 따라 포트 삭제

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 sudo docker stop win10-in-docker
 sudo docker run --name win10-in-docker \
 	    --network=host \
-            -p 5900:5900 \
             --privileged \
             -v /lib/modules:/lib/modules \
             -v /dev:/dev \
             -v $PWD/win10.iso:/home/arch/win10_x64.iso 	\
             -v $PWD/win10_hdd.img:/home/arch/win10_hdd.img \
-            --rm \
+            --rm -d \
             win10
